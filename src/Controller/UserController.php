bd201f4d5367837c0b0615564144467bcd918481
CorentinChalons <chalons.corentin@gmail.com>
2022-01-19 19:53:19 +0100

    Create Html base and scss base

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/espace-client', name: 'user_espace')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
