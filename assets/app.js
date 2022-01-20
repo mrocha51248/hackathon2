99fa03088144351ddede7b1636a9426d238a97c2
CorentinChalons <chalons.corentin@gmail.com>
2022-01-20 10:13:14 +0100

    Mario check js

--- assets/app.js
+++ assets/app.js
@@ -14,3 +14,39 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 
 import "@fortawesome/fontawesome-free/js/all.js";
 import "@fortawesome/fontawesome-free/css/all.css";
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+let dots = document.querySelectorAll(".dot");
+
+for (let i = 0; i < dots.length; i++) {
+    const dot = dots[i];
+    dot.addEventListener('click', function() {showSlides(i + 1)})
+}
